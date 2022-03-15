b60962dd8f4d1916cc293c12ce1736916f486b39
Sedar Olmez <solmez@turing.ac.uk>
2022-03-15 14:11:10 +0000

    Edited Readme.

--- Model/environment/mesa_model.py
+++ Model/environment/mesa_model.py
@@ -659,7 +659,6 @@ class MesaModel(Model):
         # Shocks to the market at a specific timestep.
         if(self.current_step == int(self.intervention_step)):
             if(self.scenario == "ratefall"):
-                print("ratefall!!!")
                 self.InterestRate = 10.0
             elif(self.scenario == "ltv"):
                 self.MaxLoanToValue = 60
